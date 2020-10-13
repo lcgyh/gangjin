943afdd3308d5a9452639a4dc289bb3422c76196
sunnyle <lic@deepblueai.com>
2020-10-14 00:28:54 +0800

    add project

---
+++ pages/index/index.js
@@ -0,0 +1,60 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    showNav:true,
+    nvabarData: {
+      showCapsule: false, //是否显示左上角图标   1表示显示    0表示不显示
+      title: '', //导航栏 中间的标题
+      bgcolor: '#fff',
+      textcolor: '#161C35',
+      showSelf:true
+    },
+    height: app.globalData.statusBarHeight * 2 + 24,
+    canIUse:true
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
